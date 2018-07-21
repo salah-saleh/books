837570c194f240033a8bd3c6f8db72c955194548
Salah Saleh <salah.saleh.ali@gmail.com>
2018-07-21 15:02:23 +0200

    rework home bage to show categories all.

--- src/components/book-home.js
+++ src/components/book-home.js
@@ -8,41 +8,164 @@ Code distributed by Google as part of the polymer project is also
 subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
 */
 
-import { LitElement, html } from '@polymer/lit-element';
+import { html } from '@polymer/lit-element';
+import { PageViewElement } from './page-view-element.js';
+import { repeat } from 'lit-html/lib/repeat.js';
+import { connect } from 'pwa-helpers/connect-mixin.js';
+import { updateMetadata } from 'pwa-helpers/metadata.js';
 
-import './book-image.js';
+import { BookButtonStyle } from './shared-styles.js';
+import { closeIcon } from './book-icons.js';
+import './book-item.js';
+import './book-offline.js';
 
-class BookHome extends LitElement {
-  _render() {
+// This element is connected to the redux store.
+import { store } from '../store.js';
+
+import { refreshPage } from '../actions/app.js';
+import { saveFavorite } from '../actions/favorites.js';
+import { favorites, favoriteListSelector } from '../reducers/favorites.js';
+
+// We are lazy loading its reducer.
+store.addReducers({
+  favorites
+});
+
+class BookHome extends connect(store)(PageViewElement) {
+  _render({ _items, _authInitialized, _user, _showOffline }) {
+    updateMetadata({
+      title: 'My Favorites - Books',
+      description: 'My Favorites'
+    });
+
     return html`
+      ${BookButtonStyle}
       <style>
         :host {
           display: block;
         }
 
-        .books-bg {
-          height: 300px;
-          max-width: 570px;
+        .books {
+          max-width: 432px;
           margin: 0 auto;
+          padding: 8px;
+          box-sizing: border-box;
+          /* remove margin between inline-block nodes */
+          font-size: 0;
         }
 
-        .books-desc {
-          padding: 24px 16px 0;
+        li {
+          display: inline-block;
+          position: relative;
+          width: calc(100% - 16px);
+          max-width: 400px;
+          min-height: 240px;
+          margin: 8px;
+          font-size: 14px;
+          vertical-align: top;
+          background: #fff;
+          border-radius: 2px;
+          box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.14),
+                      0 1px 5px 0 rgba(0, 0, 0, 0.12),
+                      0 3px 1px -2px rgba(0, 0, 0, 0.2);
+          list-style: none;
+        }
+
+        li::after {
+          content: '';
+          display: block;
+          padding-top: 65%;
+        }
+
+        h3 {
           text-align: center;
+          font-size: 24px;
+          font-weight: 400;
+          margin-bottom: 0;
+        }
+
+        .signin-section {
+          text-align: center;
+        }
+
+        .fav-button {
+          width: 32px;
+          height: 32px;
+          padding: 2px;
+          margin: 0;
+          border: 2px solid;
+          background: transparent;
+          -webkit-appearance: none;
+          cursor: pointer;
+        }
+
+        .fav-button > svg {
+          width: 24px;
+          height: 24px;
+        }
+
+        .favorites-empty {
+          text-align: center;
+        }
+
+        [hidden] {
+          display: none !important;
         }
 
         /* Wide Layout */
         @media (min-width: 648px) {
-          .books-desc {
-            padding: 96px 16px 0;
+          li {
+            height: 364px;
+          }
+        }
+
+        /* Wider layout: 2 columns */
+        @media (min-width: 872px) {
+          .books {
+            width: 832px;
+            max-width: none;
+            padding: 16px 0;
           }
         }
       </style>
 
-      <book-image class="books-bg" alt="Books Home" center src="images/books-bg.jpg" placeholder="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAQAAAACCAIAAADwyuo0AAAAI0lEQVR4AWPw2v7Wfe1Dj7X3/Pd8YPDf+Uqva79x38GQvW8Bu0sOexptskUAAAAASUVORK5CYII="></book-image>
+      <section hidden?="${_showOffline}">
+        <div class="favorites-section" hidden?="${!_user}">
+          <div class="favorites-empty" hidden?="${!_items || _items.length}">
+            <h3>No Books Available.</h3>
+          </div>
+          <ul class="books">
+            ${_items && repeat(_items, (item) => html`
+              <li>
+                <book-item item="${item}">
+                  <button class="fav-button" hidden?="${!_user}" title="Remove book" on-click="${(e) => this._removeFavorite(e, item)}">${closeIcon}</button>
+                </book-item>
+              </li>
+            `)}
+          </ul>
+        </div>
+      </section>
 
-      <div class="books-desc">Search the world's most comprehensive index of full-text books.</div>
+      <book-offline hidden?="${!_showOffline}" on-refresh="${() => store.dispatch(refreshPage())}"></book-offline>
     `;
+  }
+
+  static get properties() { return {
+    _items: Array,
+    _user: Object,
+    _showOffline: Boolean
+  }}
+
+  // This is called every time something is updated in the store.
+  _stateChanged(state) {
+    this._items = favoriteListSelector(state);
+    this._user = state.auth.user;
+    this._showOffline = state.app.offline && state.favorites.failure;
+  }
+
+  _removeFavorite(e, item) {
+    e.preventDefault();
+    store.dispatch(saveFavorite(item, true));
   }
 }
 
